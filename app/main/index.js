5524613b7a924db2f66649098d143a95ecc74fa2
kitety <aizaizuori@foxmail.com>
2020-11-15 18:22:23 +0800

    feat: init

---
+++ app/main/index.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+let win
+app.on('ready', () => {
+  win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true//允许使用node相关的
+    }
+  })
+  if (isDev) {
+    win.loadURL('http://localhost:3000')
+  } else {
+    win.loadFile(path.resolve(__dirname, '../renderer/pages/mian/index.html'))
+  }
+})
